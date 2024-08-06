5f933c0dbf62e81e78655a539fcd9bf4bcf7f5e5
Yevhen Khreptun <khreptun.e@gmail.com>
2024-08-06 10:36:55 -0400

    add test templates

--- routes/auth.py
+++ routes/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
@@ -12,5 +12,5 @@ def logout():
 
 @auth.route("/sign-in")
 def sign_in():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
 
--- routes/views.py
+++ routes/views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return "<p>Home</p>"
+    return render_template("base.html")
 
 @views.route("/profile")
 def profile():
-    return "<p>Profile</p>"
+    return render_template("profile.html")
+
+# @views.route("/test")
+# def index():
+#     return render_template("index.html")
