0b587ffcac0b6037483575ae18be2e7a68974be4
Yevhen Khreptun <khreptun.e@gmail.com>
2024-08-07 20:47:36 -0400

    add flashes to the register form

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.secret_key = b'_5#y2L"F4Q8z'
+
     from routes.views import views
     from routes.auth import auth
 
--- routes/auth.py
+++ routes/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -10,7 +10,25 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route("/sign-in")
+@auth.route("/sign-in", methods=["GET", "POST"])
 def sign_in():
+
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        user_email = request.form.get("email")
+        user_password = request.form.get("password")
+        user_confirm_password = request.form.get("confirm_password")
+
+        if len(user_name) < 2:
+            flash("Name should be more than 1 char", category="error")
+        elif len(user_email) < 4:
+            flash("Email should be more than 3 char", category="error")
+        elif len(user_password) < 7:
+            flash("Password should be more than 6 char", category="error")
+        elif len(user_password) != len(user_confirm_password):
+            flash("Passwords should be equal", category="error")
+        else:
+            flash("The account has been created!", category="success")
+
     return render_template("sign_up.html")
 
